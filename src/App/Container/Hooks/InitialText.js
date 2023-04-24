bc517e9e2e9db5c692006717cb4adb7db8f98502
Animesh Roy <mail@anir0y.in>
2023-04-24 12:54:24 +0530

    .

--- src/App/Container/Hooks/InitialText.js
+++ src/App/Container/Hooks/InitialText.js
@@ -1,7 +1,19 @@
 export const initialText = `# Markdown2PDF 
+https://m2pdf.netlify.app/
+> Awesome Markdown to PDF!
+\`\`\`diff
+- Online? Upload resume.md to stranger server?
++ Try Offline Web App!
+\`\`\`
 
-```
-You knwon the drill
-```
+## How to use md2pdf?
+1. Click button choose \`.md\` file.
+2. Edit in editor (left panel).
+3. Click **Transform**!
+4. Switch 'Destination' to **Save as PDF**.
+4. **Chrome recommended**
+
+
+
 
 `;
